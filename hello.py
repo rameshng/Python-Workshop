798380a0f747d6438dccebeed5417f01616d7ba0
rameshng <rameshng@gmail.com>
2018-10-25 14:41:29 +0530

    cipied lalit code

--- hello.py
+++ hello.py
@@ -4,3 +4,14 @@ b = 2
 c = 3
 d = a+b+c
 print (d)
+
+#Learning basic functions 
+
+#Learning on the Input. 
+answer = input("What is the air-speed velocity of an unladen swallow? ")
+What is the air-speed velocity of an unladen swallow? Huh? I... I don't know that.
+ answer 'Huh? I dont know that'
+ print ('Huh? I... I don't know that.')   answer 
+
+
+
